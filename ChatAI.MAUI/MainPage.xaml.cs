476516a3b8bc4bf56c9b91bf46595b063d3ad199
k-taro56 <121674121+k-taro56@users.noreply.github.com>
2024-03-09 04:10:32 +0900

    Add new files and components

---
+++ ChatAI.MAUI/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatAI.MAUI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ChatAI.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace ChatAI.MAUI;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
